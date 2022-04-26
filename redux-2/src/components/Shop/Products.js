4d024e698a057313662506c55519c3430ee4301a
pankaj kumar <raipankaj150@gmail.com>
2022-04-27 00:10:52 +0530

    update

--- redux-2/src/components/Shop/Products.js
+++ redux-2/src/components/Shop/Products.js
@@ -2,15 +2,40 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const DUMMY_PRODUCTS = [
+    {
+      id:'p1',
+      price:7,
+      title:'My First Book',
+      description:'This is a first product - amazing!'
+    },
+    {
+      id:'p2',
+      price:5,
+      title:'MY Second Book',
+      description:'This is a second product - amazing!'
+    },
+    {
+      id:'p3',
+      price:9,
+      title:'My Third Book',
+      description:'This is a third product - amazing!'
+    },
+  ]
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {
+          DUMMY_PRODUCTS.map(product=> <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        />)
+        }
+       
       </ul>
     </section>
   );
--- redux-2/src/store/cart-slice.js
+++ redux-2/src/store/cart-slice.js
@@ -11,10 +11,10 @@ const cartSlice = createSlice({
         addItemToCart(state,actions){
            const newItem = actions.payload;
            const existingItem = state.items.find(item=>item.id===newItem.id);
-
+           state.totalQuantity++;
            if(!existingItem){
                state.items.push({
-                   itemId:newItem.id,
+                   id:newItem.id,
                    price:newItem.price,
                    quantity:1,
                    totalPrice:newItem.price,
@@ -28,7 +28,7 @@ const cartSlice = createSlice({
         removeItemFromCart(state,actions){
             const id = actions.payload;
             const existingItem = state.items.find(item=>item.id===id);
-
+            state.totalQuantity--;
             if(existingItem.quantity===1){
                 state.items = state.items.filter(item=>item.id!==id); 
             }else{
